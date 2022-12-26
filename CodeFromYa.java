bea218d59f765298bf09b69bf6f464004cef9f77
AlexDynk <dynkdyn@gmail.com>
2022-12-26 19:48:16 +0200

    Мой первый коммит

---
+++ CodeFromYa.java
@@ -0,0 +1,29 @@
+package ru.inald39.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		float i = 6;
+		float j = 7;
+		float answer = i / j;
+		
+		String message = "Hello world!";
+		String stringAnswer = message + " " + answer;
+		
+		boolean isMoreThanOne;
+		if (answer > 1) {
+			isMoreThanOne = true;
+		} else {
+			isMoreThanOne = false;
+		}
+		
+		System.out.println(message);
+		System.out.println(stringAnswer + " " + isMoreThanOne);
+		
+		
+		
+	}
+
+}
+s
