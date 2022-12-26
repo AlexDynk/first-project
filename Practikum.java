a21d82658360124f1acd9bb8dfae90cd4c940111
AlexDynk <dynkdyn@gmail.com>
2022-12-26 20:05:16 +0200

    Ввел код для отображения Hello world и сравнение числа с 1

---
+++ Practikum.java
@@ -0,0 +1,26 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		float i = 6;
+		float j = 7;
+		float answer = i / j;
+		
+		String message = "Hello world!";
+		String stringAnswer = message + " " + answer;
+		
+		boolean isMoreThanOne;
+		if (answer > 1) {
+			isMoreThanOne = true;
+		} else {
+			isMoreThanOne = false;
+		}
+		
+		System.out.println(message);
+		System.out.println(stringAnswer + " " + isMoreThanOne);
+		
+		
+		
+	}
+
+}
